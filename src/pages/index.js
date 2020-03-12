97d0ee89e0ee1f4ce14537e26bc0d62a8fa34e0a
Roy <roygufo@gmail.com>
2020-03-12 16:24:50 -0600

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello, Roy.</h1>
+      <h2>Welcome to your awesome first website.</h2>
+    </div>
+  )
+}
+
+export default IndexPage
